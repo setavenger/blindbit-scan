e0880d16ee15f457473d72dc47f2351e388906d6
setavenger <github@snblago.com>
2025-04-24 20:57:05 +0200

    Check for nil event before accessing
    fix #4
    some cleanup

--- pkg/networking/nwc.go
+++ pkg/networking/nwc.go
@@ -33,13 +33,6 @@ type GetBalanceResponse struct {
 	Balance int64 `json:"balance"` // in millisatoshis
 }
 
-const (
-	privKeyWalletService = "66d1069f89c77d846cca006d132f2493a0acd31f650e000bc5199306556d8b21"
-	pubKeyWalletService  = "3e6221b76a6555c819c8d0c87d9af47ce1b31c0b413f276b2f0d8ddd2a3e1c1f"
-	privKeyClientSecret  = "125b81e4b8b21c889374ebf1a4b786791f0088a4f3b64260e75fd759497c2700"
-	pubKeyClient         = "e89262ebbe2b114bfa1f53b0b65a46a9bdf65ac2c11a7969933c9aac8a4e83dd"
-)
-
 // publishInfoEvent publishes a replaceable info event (kind 13194) to the relay.
 func publishInfoEvent(ctx context.Context, relay *nostr.Relay, privKey, pubKey string) {
 	// Supported commands as a space-separated string.
